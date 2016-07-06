4ae389bfbd9770d93606a42a598a6a7a151404e7
Sperlhofer <j.sperlhofer@benedit.at>
2016-07-06 13:54:39 +0200

    fading 2

--- pirled/pirled.ino
+++ pirled/pirled.ino
@@ -1,4 +1,87 @@
 
+/*
+* Code for cross-fading 3 LEDs, red, green and blue (RGB) 
+* To create fades, you need to do two things: 
+*  1. Describe the colors you want to be displayed
+*  2. List the order you want them to fade in
+*
+* DESCRIBING A COLOR:
+* A color is just an array of three percentages, 0-100, 
+*  controlling the red, green and blue LEDs
+*
+* Red is the red LED at full, blue and green off
+*   int red = { 100, 0, 0 }
+* Dim white is all three LEDs at 30%
+*   int dimWhite = {30, 30, 30}
+* etc.
+*
+* Some common colors are provided below, or make your own
+* 
+* LISTING THE ORDER:
+* In the main part of the program, you need to list the order 
+*  you want to colors to appear in, e.g.
+*  crossFade(red);
+*  crossFade(green);
+*  crossFade(blue);
+*
+* Those colors will appear in that order, fading out of 
+*    one color and into the next  
+*
+* In addition, there are 5 optional settings you can adjust:
+* 1. The initial color is set to black (so the first color fades in), but 
+*    you can set the initial color to be any other color
+* 2. The internal loop runs for 1020 interations; the 'wait' variable
+*    sets the approximate duration of a single crossfade. In theory, 
+*    a 'wait' of 10 ms should make a crossFade of ~10 seconds. In 
+*    practice, the other functions the code is performing slow this 
+*    down to ~11 seconds on my board. YMMV.
+* 3. If 'repeat' is set to 0, the program will loop indefinitely.
+*    if it is set to a number, it will loop that number of times,
+*    then stop on the last color in the sequence. (Set 'return' to 1, 
+*    and make the last color black if you want it to fade out at the end.)
+* 4. There is an optional 'hold' variable, which pasues the 
+*    program for 'hold' milliseconds when a color is complete, 
+*    but before the next color starts.
+* 5. Set the DEBUG flag to 1 if you want debugging output to be
+*    sent to the serial monitor.
+*
+*    The internals of the program aren't complicated, but they
+*    are a little fussy -- the inner workings are explained 
+*    below the main loop.
+*
+* April 2007, Clay Shirky <clay.shirky@nyu.edu> 
+*/ 
+
+// Output
+int redPin = 9;   // Red LED,   connected to digital pin 9
+int grnPin = 10;  // Green LED, connected to digital pin 10
+int bluPin = 11;  // Blue LED,  connected to digital pin 11
+
+// Color arrays
+int black[3]  = { 0, 0, 0 };
+int white[3]  = { 100, 100, 100 };
+int red[3]    = { 100, 0, 0 };
+int green[3]  = { 0, 100, 0 };
+int blue[3]   = { 0, 0, 100 };
+int yellow[3] = { 40, 95, 0 };
+int dimWhite[3] = { 30, 30, 30 };
+// etc.
+
+// Set initial color
+int redVal = black[0];
+int grnVal = black[1]; 
+int bluVal = black[2];
+
+int wait = 10;      // 10ms internal crossFade delay; increase for slower fades
+int hold = 0;       // Optional hold when a color is complete, before the next crossFade
+int DEBUG = 1;      // DEBUG counter; if set to 1, will write values back via serial
+int loopCount = 60; // How often should DEBUG report?
+int j = 0;          // Loop counter for repeat
+
+// Initialize color variables
+int prevR = redVal;
+int prevG = grnVal;
+int prevB = bluVal;
 
 
 /* 
@@ -113,7 +196,27 @@ void loop(){
   }
 
 
+// Set up the LED outputs
+void setup_cf()
+{
+  pinMode(redPin, OUTPUT);   // sets the pins as output
+  pinMode(grnPin, OUTPUT);   
+  pinMode(bluPin, OUTPUT); 
 
+  if (DEBUG) {           // If we want to see values for debugging...
+    Serial.begin(9600);  // ...set up the serial ouput 
+  }
+}
+
+// Main program: list the order of crossfades
+void loop_cf()
+{
+  crossFade(red);
+  crossFade(green);
+  crossFade(blue);
+  crossFade(yellow);
+
+}
 
 /* BELOW THIS LINE IS THE MATH -- YOU SHOULDN'T NEED TO CHANGE THIS FOR THE BASICS
 * 
